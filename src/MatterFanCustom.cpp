bd00bee635dfbac42993bc6624d1885d9f22c24a
Timothy Perrett <125782756+TimothyPPerrett@users.noreply.github.com>
2024-12-28 16:27:02 +0800

    Improved copyright/licensing info

--- src/MatterFanCustom.cpp
+++ src/MatterFanCustom.cpp
@@ -1,9 +1,11 @@
 /*
- * This file is part of the Silicon Labs Arduino Core
+ * This file has been modified from MatterFan.cpp, 
+ * a part of the Silicon Labs Arduino Core
  *
  * The MIT License (MIT)
  *
  * Copyright 2024 Silicon Laboratories Inc. www.silabs.com
+ * Copyright 2024 Timothy Perrett
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
