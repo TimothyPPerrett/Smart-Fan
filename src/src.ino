5c85a3384c9408fb38146e266c39c865216ed6e2
Timothy Perrett <125782756+TimothyPPerrett@users.noreply.github.com>
2025-01-09 08:28:23 +0800

    Temperature sensor integration

--- src/src.ino
+++ src/src.ino
@@ -29,6 +29,7 @@
 
 #include <stdint.h>
 #include <Matter.h>
+#include <MatterTemperature.h>
 #include "MatterFanCustom.h"
 
 /// @brief Enum representing fan speed settings.
@@ -68,11 +69,47 @@ const uint8_t kMediumButtonPin = D7;
 const uint8_t kHighButtonPin = D10;
 #endif
 
+/// @brief Pin the temp sensor is connected to. A0 on the board.
+const uint8_t kTempSensorPin = A5;
+
+/// @brief Minimum interval, in milliseconds, between temp sensor readings
+const unsigned long kTempSensorInterval = 10000UL;
+/// @brief Reading of millis() last time the temp sensor was read
+unsigned long previousTime = 0UL;
+
+// Chosen analog reference
+// AR_INTERNAL1V2 gives a -50 to 70 C range for the TMP36
+#define AR_CHOICE AR_INTERNAL1V2
+
+#if AR_CHOICE == AR_INTERNAL1V2
+/// @brief Analog reference voltage, in millivolts.
+/// Gives a -50 to 70 C range for the TMP36
+const int16_t kARVoltage = 1200;
+#elif AR_CHOICE == AR_EXTERNAL_1V25
+/// @brief Analog reference voltage, in millivolts
+/// Gives a -50 to 75 C range for the TMP36
+const int16_t kARVoltage = 1250;
+#endif
+
+
+/// @brief Temp sensor calibrated temp in degrees C
+const int16_t kTempSensorCalibrated = 25;
+/// @brief Temp sensor scale in mV/C
+const int16_t kTempSensorVScale = 10;
+/// @brief Output voltage in millivolts of the temp sensor at the calibrated temperature
+const int16_t kTempSensorVoltageCalibrated = 750;
+/// @brief Temperature corresponding to a temp sensor reading of 0V
+const int16_t kTempSensorTemp0V = kTempSensorCalibrated - kTempSensorVoltageCalibrated / kTempSensorVScale;
+/// @brief Temperature corresponding to a temp sensor reading of kARVoltage
+const int16_t kTempSensorTemp1V2 = kTempSensorCalibrated + (kARVoltage - kTempSensorVoltageCalibrated) / kTempSensorVScale;
+
 /// @brief FanState the fan's hardware is set to.
 volatile FanState fan_hardware_state = FanState::Off;
 
 /// @brief Object exposed to Matter. Tracks software state of the fan.
 MatterFanCustom matter_fan;
+MatterTemperature matter_temp_sensor;
+MatterTemperature matter_cpu_temp_sensor;
 
 StaticSemaphore_t matter_device_event_semaphore_buf;
 SemaphoreHandle_t matter_device_event_semaphore;
@@ -81,6 +118,8 @@ SemaphoreHandle_t matter_device_event_semaphore;
 void updateFanState();
 void setFanSpeed(FanState speed);
 String fanStateToString(FanState state);
+void updateTempSensor();
+void updateCpuTempSensor();
 void matterFanChangeCallback();
 
 void setup()
@@ -124,6 +163,8 @@ void setup()
   noInterrupts();
   #endif
 
+  analogReference(AR_INTERNAL1V2);
+
   // Create a binary semaphore
   matter_device_event_semaphore = xSemaphoreCreateBinaryStatic(&matter_device_event_semaphore_buf);
 
@@ -132,16 +173,28 @@ void setup()
   Serial.begin(115200);
   Matter.begin();
   matter_fan.begin();
+  matter_temp_sensor.begin();
+  matter_cpu_temp_sensor.begin();
 
   matter_fan.set_device_name("Matter Fan");
   matter_fan.set_vendor_name("Homemaker/Sparkfun");
   matter_fan.set_product_name("HMAWP-4097/Thing Plus Matter");
   matter_fan.set_serial_number(getDeviceUniqueIdStr().c_str());
 
+  matter_temp_sensor.set_device_name("Room Temperature Sensor");
+  matter_temp_sensor.set_vendor_name("Analog Instruments/Sparkfun");
+  matter_temp_sensor.set_product_name("TMP36/Thing Plus Matter");
+  matter_temp_sensor.set_serial_number(getDeviceUniqueIdStr().c_str());
+
+  matter_cpu_temp_sensor.set_device_name("CPU Temperature Sensor");
+  matter_cpu_temp_sensor.set_vendor_name("Sparkfun");
+  matter_cpu_temp_sensor.set_product_name("Thing Plus Matter");
+  matter_cpu_temp_sensor.set_serial_number(getDeviceUniqueIdStr().c_str());
+
   matter_fan.set_device_change_callback(matterFanChangeCallback);
 
   #if DEBUG | DEBUG_MATTER
-  Serial.println("Matter fan");
+  Serial.println("Matter fan and temperature sensor");
 
   if (!Matter.isDeviceCommissioned()) {
     Serial.println("Matter device is not commissioned");
@@ -165,7 +218,7 @@ void setup()
 
   Serial.println("Waiting for Matter device discovery...");
   #endif
-  while (!matter_fan.is_online()) {
+  while (!matter_fan.is_online() | !matter_temp_sensor.is_online() | !matter_cpu_temp_sensor.is_online()) {
     delay(200);
   }
   #if DEBUG | DEBUG_MATTER
@@ -180,12 +233,58 @@ void setup()
 
 void loop()
 {
-  xSemaphoreTake(matter_device_event_semaphore, portMAX_DELAY);
-  #if DEBUG
-  Serial.println("Semaphore taken. Updating fan mode.");
-  #endif
+  unsigned long currentTime = millis();
+  unsigned long elapsedTime = currentTime - previousTime;
+  
+  if (elapsedTime >= kTempSensorInterval)
+  {
+    // Temp sensor polling interval has already expired.
+    #if DEBUG
+    Serial.println("Temp sensor interval elapsed in loop");
+    #endif
+    
+    // xBlockTime of 0, then immediately poll the sensor
+    if (xSemaphoreTake(matter_device_event_semaphore, 0)  == pdTRUE)
+    {
+      #if DEBUG
+      Serial.println("Semaphore taken. Updating fan mode.");
+      #endif
+      updateFanState();
+    }
 
-  updateFanState();
+    updateTempSensor();
+    updateCpuTempSensor();
+    previousTime = millis();
+  }
+  else
+  {
+    // Temp sensor polling interval is yet to expire.
+
+    #if DEBUG
+    Serial.println("Temp sensor interval not yet elapsed");
+    #endif
+
+    // xBlockTime equal to the remaining time in the interval
+    TickType_t xTempSensorTicksToWait = pdMS_TO_TICKS(kTempSensorInterval - elapsedTime);
+    if (xSemaphoreTake(matter_device_event_semaphore, xTempSensorTicksToWait) == pdTRUE)
+    {
+      // Semaphore taken means we've resumed before the interval expired
+      #if DEBUG
+      Serial.println("Semaphore taken. Updating fan mode.");
+      #endif
+      updateFanState();
+    }
+    else
+    {
+      // Semaphore not taken means the interval has expired
+      #if DEBUG
+      Serial.println("Temp sensor interval elapsed while awaiting semaphore.");
+      #endif
+      updateTempSensor();
+      updateCpuTempSensor();
+      previousTime = millis();
+    }
+  }
 }
 
 /// @brief Synchronise the physical fan's on/off state with that of the Matter fan
@@ -270,6 +369,56 @@ void matterFanChangeCallback()
   Serial.println("Matter change callback triggered.");
   #endif
   xSemaphoreGive(matter_device_event_semaphore);
+}
+
+float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
+{
+  return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
+}
+
+float readTempSensorRaw()
+{
+  // TMP36 has a scale factor of 10mV/C
+  // and outputs 750mV at 25C.
+  // The 0 to 3.3V range readable on the analog input pins
+  // then maps to temperatures from -50 to 280.
+
+  int rawInput = analogRead(kTempSensorPin);
+  #if DEBUG
+  Serial.print("Raw temp input: ");
+  Serial.println(rawInput);
+  #endif
+
+  // Temperatures scaled by 100 so we can plug 
+  // them straight into set_measured_value_raw
+  return mapfloat(rawInput, 0, 4096,
+    kTempSensorTemp0V * 100, kTempSensorTemp1V2 * 100);
+}
+
+void updateTempSensor()
+{
+  #if DEBUG
+  Serial.println("Measuring temperature.");
+  #endif
+  matter_temp_sensor.set_measured_value_raw(readTempSensorRaw());
+  #if DEBUG
+  Serial.print("Temperature: ");
+  Serial.print(matter_temp_sensor.get_measured_value_celsius());
+  Serial.println(" C");
+  #endif
+}
+
+void updateCpuTempSensor()
+{
+  #if DEBUG
+  Serial.println("Measuring CPU temperature.");
+  #endif
+  matter_cpu_temp_sensor.set_measured_value_celsius(getCPUTemp());
+  #if DEBUG
+  Serial.print("Temperature: ");
+  Serial.print(matter_cpu_temp_sensor.get_measured_value_celsius());
+  Serial.println(" C");
+  #endif
 }
 
 #if BUTTONS
